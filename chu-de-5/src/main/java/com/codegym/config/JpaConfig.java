2b66a6077fa459661744fae0718551f826ff99e9
HK-204 <haidao2004bt@gmail,com>
2025-11-14 20:03:50 +0700

    update

--- chu-de-5/src/main/java/com/codegym/config/JpaConfig.java
+++ chu-de-5/src/main/java/com/codegym/config/JpaConfig.java
@@ -3,6 +3,7 @@ package com.codegym.config;
 import jakarta.persistence.EntityManagerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -12,6 +13,7 @@ import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
+@EnableJpaRepositories(basePackages = "com.codegym.repository")
 public class JpaConfig {
 
     @Bean
